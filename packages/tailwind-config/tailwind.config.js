ccee3adb49ac69fdceec1609390c08197d297c5c
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-05-27 23:48:49 +0700

    tailwind-config: shared tailwind configuration

---
+++ packages/tailwind-config/tailwind.config.js
@@ -0,0 +1,19 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    // sveltekit app content
+    `src/**/*.{html,js,svelte,ts}`,
+    // include packages if not transpiling
+    // "../../packages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brandblue: colors.blue[500],
+        brandred: colors.red[500],
+      },
+    },
+  },
+  plugins: [],
+};
