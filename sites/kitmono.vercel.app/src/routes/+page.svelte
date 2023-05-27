de30d19fbf13e4ff306babb843edcbdb1294a6e2
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-05-28 01:29:26 +0700

    kitmono.vercel.app: example use ui

--- sites/kitmono.vercel.app/src/routes/+page.svelte
+++ sites/kitmono.vercel.app/src/routes/+page.svelte
@@ -1,17 +1,24 @@
 <script>
 	import Greeting from '../lib/components/Greeting.svelte';
 	import GreetingShared from '../lib/components/GreetingShared.svelte';
+	import { MyCounterButton } from 'ui';
 </script>
 
-<!-- class come from tailwind-config -->
-<h1 class="text-brandblue">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<div class="flex flex-col gap-5 p-5 bg-blue-100">
+<div class="flex flex-col gap-10">
 	<div>
-		<Greeting />
+		<!-- class come from tailwind-config -->
+		<h1 class="text-brandblue">Welcome to SvelteKit</h1>
+		<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	</div>
+	<div class="flex flex-col gap-5 p-5 bg-blue-100">
+		<div>
+			<Greeting />
+		</div>
+		<div>
+			<GreetingShared />
+		</div>
 	</div>
 	<div>
-		<GreetingShared />
+		<MyCounterButton />
 	</div>
 </div>
