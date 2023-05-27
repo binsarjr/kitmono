006d424c5726df9cc99015dbd722a17388ce6ebd
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-05-28 01:05:45 +0700

    kitmono.vercel.app: add tailwind-config workspace

--- sites/kitmono.vercel.app/src/routes/+page.svelte
+++ sites/kitmono.vercel.app/src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
+<!-- class come from tailwind-config -->
+<h1 class="text-brandblue">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
